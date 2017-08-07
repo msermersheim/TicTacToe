1d17a5f9061889274f8387547fb558c818180000
Michael Sermersheim <m.sermersheim@gmail.com>
2017-08-06 18:14:55 -0700

    Functioning game

--- index.js
+++ index.js
@@ -2,9 +2,25 @@ var prompt = require('prompt');
 
 prompt.start();
 
-// var startGame = function() {
-// var currentPlayer = 'X';
-// };
+var checkWinner = function(board, player, row, col) {
+  // check horizontal
+  if (board[row][(col + 2) % 3] === player && board[row][(col + 1) % 3] === player) {
+    return true;
+  }
+  // check vertical
+  if (board[(row + 2) % 3][col] === player && board[(row + 1) % 3][col] === player) {
+    return true;
+  }
+  // check major diag
+  if (row === col && board[(row + 2) % 3][(col + 2) % 3] === player && board[(row + 1) % 3][(col + 1) % 3] === player) {
+    return true;
+  }
+  // check minor diag
+  if (row + col === 2 && board[(row + 2) % 3][(col + 1) % 3] === player && board[(row + 1) % 3][(col + 2) % 3] === player) {
+    return true;
+  }
+  return false;
+};
 
 var printBoard = function(board) {
   console.log(board[0][0], '|', board[0][1],'|', board[0][2]);
@@ -25,34 +41,36 @@ var takeTurn = function(currentPlayer, board) {
   console.log('Player ', currentPlayer, ', pick square number (1-9)');
   prompt.get(['square'], function (err, result) {
     if (err) { return onErr(err); }
-    // error handling for number not 1-9;
-    else {
+    if (!(result.square > 0 && result.square < 10)) {
+      console.log('Please choose from 1-9');
+      takeTurn(currentPlayer, board);
+    } else {
       var row = Math.floor((result.square - 1) / 3);
       var col = (result.square - 1) % 3;
-      board[row][col] = currentPlayer;
-      // printBoard(board);
-      currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
-      takeTurn(currentPlayer, board);
+      if(board[row][col] !== ' ') {
+        console.log('Please select an empty square');
+        takeTurn(currentPlayer, board);
+      } else {
+        board[row][col] = currentPlayer;
+        if (checkWinner(board, currentPlayer, row, col)) {
+          printBoard(board);
+          console.log('Player ', currentPlayer, ' wins!');
+          console.log('Play again? (y/n)');
+          prompt.get(['answer'], function (err, result) {
+            if (err) { return onErr(err); }
+            if (result.answer.toLowerCase() === 'y') {
+              startGame();
+            } else {
+              console.log('Thanks for playing!');
+            }
+          });
+        } else {
+          currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+          takeTurn(currentPlayer, board);
+        }
+      }
     }
   });
-}
+};
 
 startGame();
-
-
-
-
-
-
-
-
-
-
-// prompt.get(['username', 'email'], function (err, result) {
-//     // 
-//     // Log the results. 
-//     // 
-//     console.log('Command-line input received:');
-//     console.log('  username: ' + result.username);
-//     console.log('  email: ' + result.email);
-//   });
